921ee0b8da2c89b658cea07c81b4dc0770c32843
Xavi Esteve <xavi@xaviesteve.com>
2024-05-05 15:41:32 +0200

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -1,55 +1,101 @@
 # This is a list of commands I run on a freshly installed Ubuntu/Debian
 # NOTE: It is not meant to be ran as a .sh file, but to copy/paste each line separately
 
+# =======================
+# ====== Must-haves =====
+# =======================
+
+sudo apt update && sudo apt upgrade -y
+
 # Make Downloads folders
 mkdir ~/Downloads/Temp
 mkdir ~/Downloads/Keep
 
-sudo apt update
-sudo apt upgrade -y
-
+# 🔵 SSH, Fail2Ban and UFW
 sudo apt install openssh-server fail2ban ufw
-sudo apt install npx nodejs
-
 sudo ufw allow ssh
 sudo ufw allow http
 sudo ufw allow https
 sudo ufw enable
-sudo ufw deny from 91.109.117.228
 sudo ufw status
+#sudo ufw deny from 91.109.117.228
 
-# install syncthing https://www.linuxcapable.com/how-to-install-syncthing-on-ubuntu-linux/
+# 🔵 Syncthing
+# https://www.linuxcapable.com/how-to-install-syncthing-on-ubuntu-linux/
+sudo apt install software-properties-common apt-transport-https wget curl -y
+sudo curl -o /usr/share/keyrings/syncthing-archive-keyring.gpg https://syncthing.net/release-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
+sudo apt update && sudo apt install syncthing -y
+syncthing --version
+sudo systemctl enable syncthing@xavi.service
+sudo systemctl start syncthing@xavi.service
+sudo ufw allow syncthing
 
-# set up KDE Connect
+# 🔵 Visual Sudio Code
+sudo apt install software-properties-common apt-transport-https wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt update && sudo apt install code -y
 
-# install espanso
+# 🔵 KDE Connect
+sudo apt install kdeconnect
+sudo ufw allow 1714:1764/udp
+sudo ufw allow 1714:1764/tcp
+
+# 🔵 Espanso
 cd ~/Downloads/Temp
 wget https://github.com/federico-terzi/espanso/releases/download/v2.2.1/espanso-debian-x11-amd64.deb
 sudo apt install ./espanso-debian-x11-amd64.deb
 espanso service register
 
-# install netdata?
-# install Firefox
-# install Visual Sudio Code
-# install DBeaver-CE https://dbeaver.io/download/
-# install iriun webcam https://iriun.com/
-# install remote desktop (meshcentral, rustdesk, teamviewer, anydesk...)
-# install rustdesk https://github.com/rustdesk/rustdesk/releases
+# 🔵 DBeaver-CE https://dbeaver.io/download/
+# 🔵 Iriun Webcam https://iriun.com/
+# 🔵 Remote Desktop (MeshCentral, RustDesk, TeamViewer, AnyDesk...)
+#    - RustDesk https://github.com/rustdesk/rustdesk/releases
 
-# gaming PC? https://gitlab.com/corectrl/corectrl
+# 🔵 FileZilla, qBittorrent
 sudo apt install filezilla qbittorrent
 
-# Griffin PowerMate (you don't need this, it's a gadget I bought a long time ago and use it as a volume slider for my desktop computer + click to sleep)
-# Ubuntu
-sudo add-apt-repository ppa:stefansundin/powermate
-# Debian
-echo 'deb http://ppa.launchpad.net/stefansundin/powermate/ubuntu focal main' | sudo tee /etc/apt/sources.list.d/powermate.list && sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 0xFFE1FFFFAFEC55BB
-sudo apt update
-sudo apt install powermate
-powermate
-# Extra settings: sudo nano /etc/powermate.toml
 
-# Amazon Web Services CLI
+# =================================
+# ====== Software Development =====
+# =================================
+
+# 🔵 Node
+sudo apt install npx nodejs
+
+# 🔵 Apache
+sudo apt install -y apache2 apache2-utils composer curl
+sudo systemctl enable apache2
+sudo ln -s ~/Dropbox/WWW/ /var/www/WWW
+sudo chown www-data:www-data /var/www/ -R
+# Add user to www-data and vice-versa
+sudo usermod -a -G www-data xavi
+sudo usermod -a -G xavi www-data
+
+sudo chown -R $USER:www-data /var/www/
+
+# 🔵 MariaDB
+# sudo nano /etc/apache2/conf-available/servername.conf
+sudo apt install mariadb-server mariadb-client
+sudo systemctl start mariadb
+sudo systemctl enable mariadb
+sudo mysql_secure_installation
+sudo service apache2 reload
+# set root password
+sudo mysql
+# ALTER USER 'root'@'localhost' IDENTIFIED VIA mysql_native_password USING PASSWORD('root');
+
+# 🔵 PHP 8
+sudo apt install lsb-release ca-certificates apt-transport-https software-properties-common -y
+sudo add-apt-repository ppa:ondrej/php
+sudo apt install php8.1 php8.1-cli php8.1-common php8.1-imap php8.1-redis php8.1-snmp php8.1-xml php8.1-zip php8.1-mbstring php8.1-mysql php8.1-curl
+# find more extensions via `apt search php- | less
+
+sudo a2enmod rewrite
+
+# 🔵 Amazon Web Services CLI
 sudo apt install awscli
 aws configure
 # https://us-east-1.console.aws.amazon.com/iam/home?region=eu-west-1#/users
@@ -60,7 +106,28 @@ aws configure
 ## List: aws s3 ls s3://bucketname/
 ## Download: aws s3 cp s3://bucketname/file.txt file.txt
 
-# Battery saving
+# 🔵 Composer 
+# https://getcomposer.org/download/
+# sudo mv composer.phar /usr/local/bin/composer
+
+# 🔵 Deployer
+# https://deployer.org/docs/7.x/installation
+# remember to create the alias
+
+# Virtual hosts
+cd /etc/apache2/sites-available/
+sudo nano vhosts.conf
+sudo a2dissite 000-default.conf
+sudo a2ensite vhosts.conf
+sudo service apache2 reload
+sudo nano /etc/hosts
+
+
+# ====================
+# ====== Laptops =====
+# ====================
+
+# 🔵 Battery saving
 sudo add-apt-repository ppa:linrunner/tlp
 sudo apt update
 sudo apt install tlp tlp-rdw
@@ -74,57 +141,38 @@ sudo add-apt-repository ppa:slimbook/slimbook
 sudo apt update
 sudo apt install slimbookbattery
 
-# Apache
-sudo apt install -y apache2 apache2-utils composer curl
-sudo systemctl enable apache2
-sudo ln -s ~/Dropbox/WWW/ /var/www/WWW
-sudo chown www-data:www-data /var/www/ -R
 
-# Add user to www-data and vice-versa
-sudo usermod -a -G www-data xavi
-sudo usermod -a -G xavi www-data
+# ===================
+# ====== Gaming =====
+# ===================
 
-sudo chown -R $USER:www-data /var/www/
-
-# MariaDB
-# sudo nano /etc/apache2/conf-available/servername.conf
-sudo apt install mariadb-server mariadb-client
-sudo systemctl start mariadb
-sudo systemctl enable mariadb
-sudo mysql_secure_installation
-sudo service apache2 reload
-
-# set MariaDB root password
-sudo mysql
-# ALTER USER 'root'@'localhost' IDENTIFIED VIA mysql_native_password USING PASSWORD('root');
-
-# PHP 8
-sudo apt install lsb-release ca-certificates apt-transport-https software-properties-common -y
-sudo add-apt-repository ppa:ondrej/php
-sudo apt install php8.1 php8.1-cli php8.1-common php8.1-imap php8.1-redis php8.1-snmp php8.1-xml php8.1-zip php8.1-mbstring php8.1-mysql php8.1-curl
-# find more extensions via `apt search php- | less
-
-sudo a2enmod rewrite
-
-# install composer https://getcomposer.org/download/
-# sudo mv composer.phar /usr/local/bin/composer
-
-# install deployer https://deployer.org/docs/7.x/installation
-# remember to create the alias
-
-# Virtual hosts
-cd /etc/apache2/sites-available/
-sudo nano vhosts.conf
-sudo a2dissite 000-default.conf
-sudo a2ensite vhosts.conf
-sudo service apache2 reload
-sudo nano /etc/hosts
-
-# Gaming
+# 🔵 GameMode
 sudo apt install gamemode
 
+# 🔵 CoreCtrl (control with ease your computer hardware using application profiles)
+# https://gitlab.com/corectrl/corectrl
 
 
+# ===========================
+# ====== Other software =====
+# ===========================
+
+# 🔵 Netdata
+
+# 🔵 Griffin PowerMate (you don't need this, it's a gadget I bought a long time ago and use it as a volume slider for my desktop computer + click to sleep)
+# Ubuntu
+sudo add-apt-repository ppa:stefansundin/powermate
+# Debian
+echo 'deb http://ppa.launchpad.net/stefansundin/powermate/ubuntu focal main' | sudo tee /etc/apt/sources.list.d/powermate.list && sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 0xFFE1FFFFAFEC55BB
+sudo apt update
+sudo apt install powermate
+powermate
+# Extra settings: sudo nano /etc/powermate.toml
+
+
+# ========================
+# ====== Final Steps =====
+# ========================
 
 # Clean up and reboot
 sudo apt autoremove
