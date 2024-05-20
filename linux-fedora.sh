e40bba701051930f2bca0ba4c728815c26dc1e07
Xavi Esteve <xavi@xaviesteve.com>
2024-05-20 18:00:02 +0200

    Update linux-fedora.sh

--- linux-fedora.sh
+++ linux-fedora.sh
@@ -6,3 +6,15 @@ sudo dnf module reset php
 sudo dnf module install php:remi-8.2
 php -v
 
+## Apache
+
+sudo dnf update -y
+sudo dnf install httpd -y
+rpm -qi httpd
+systemctl start httpd
+systemctl enable httpd
+nano /etc/httpd/conf.d/vhosts.conf
+sudo nano /etc/httpd/conf.d/vhosts.conf
+systemctl restart httpd
+sudo nano /etc/hosts
+# sudo usermod -a -G <group> <user>
