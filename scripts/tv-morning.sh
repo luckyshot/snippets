6123b3b27825be0bbc2f07b1d8aef91b3cde260b
Xavi Esteve <xavi@xaviesteve.com>
2026-05-20 08:43:10 +0200

    Update tv-morning.sh

--- scripts/tv-morning.sh
+++ scripts/tv-morning.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+DISPLAY=:0 PULSE_RUNTIME_PATH=/run/user/1000/pulse
 # Kill Firefox (closes all tabs)
 pkill -f firefox
 sleep 1
- 
-# Open Firefox with jango.com
-firefox "https://www.jango.com" &
- 
-# Set master volume to 40%
+
+ # Set master volume to 40%
 pactl set-sink-volume @DEFAULT_SINK@ 40%
+
+# Open Firefox with jango.com
+xdg-open "https://www.jango.com"
