dc74653660d0d4b1c4a28e02040f305af8444e3f
Xavi Esteve <xavi@xaviesteve.com>
2026-06-06 06:32:59 +0200

    Update brightness-schedule.sh

--- scripts/brightness-schedule.sh
+++ scripts/brightness-schedule.sh
@@ -20,11 +20,11 @@ elif [ "$t" -ge 2100 ]; then
     bright 10
 elif [ "$t" -ge 2000 ]; then
     bright 50
-elif [ "$t" -ge 800 ]; then
+elif [ "$t" -ge 1000 ]; then
     bright 100
-elif [ "$t" -ge 700 ]; then
+elif [ "$t" -ge 900 ]; then
     bright 90
-elif [ "$t" -ge 600 ]; then
+elif [ "$t" -ge 800 ]; then
     bright 30
 else
     bright 0
